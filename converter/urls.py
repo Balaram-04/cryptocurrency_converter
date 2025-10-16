a2b9298264bb04d075f478dadc3f38519b61fcb6
Balaram-04 <155076342+Balaram-04@users.noreply.github.com>
2025-10-16 18:07:41 +0530

    Create urls.py

---
+++ converter/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+
+app_name = 'converter'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('api/supported/', views.supported_currencies, name='supported'),
+    path('api/convert/', views.convert, name='convert'),
+]
